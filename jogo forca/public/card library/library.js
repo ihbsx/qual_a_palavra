5e77674f2ae77546e947ff108040ea5847608c75
ihbsx <109470737+ihbsx@users.noreply.github.com>
2024-02-18 15:37:31 -0300

    Add files via upload

---
+++ jogo forca/public/card library/library.js
@@ -0,0 +1,85 @@
+let easy_list = "BEM-TE-VI, BABUÍNO"
+let normal_list = ", BABUÍNO, BACALHAU, BACURI, BADEJO, BAGRE, BAIACU, BALEIA, BARATA, BARBO, BARRACUDA, BEIJA-FLOR, BESOURO"
+let hard_list = ", CÁGADO, CAMALEÃO, CAMARÃO, CAMELO, CAMUNDONGO, CANÁRIO, CANGURU, CAPIVARA, CARACOL, CARANGUEJO, CARNEIRO, CARRAPATO, CASCAVEL, CASTOR, CAVALO, CAVALO-MARINHO, CEGONHA, CENTOPEIA, CHIMPANZÉ, CHINCHILA, CHITA, CIGARRA, CISNE, COALA, COBRA, CODORNA, COELHO, COIOTE, CORUJA, CORVO, CROCODILO, CUPIM, CUTIA"
+let mythic_list = ", DRAGÃO,  IÉTI"
+
+const library = [
+    // create diferent types ? how? (air, water, forest, insect, especial, ""mythic(dragon , etc)??"" etc...)
+    // Easy List
+    {   index: "1",
+        name: "BEM-TE-VI",
+        type: "air",
+        info_source: "https://pt.wikipedia.org/wiki/Bem-te-vi",
+        info: " Medindo cerca de 23,5 centímetros, caracteriza-se principalmente pela coloração amarela viva no ventre e uma listra branca no alto da cabeça, além do canto que nomeia o animal.<br><br> Considerado um dos pássaros mais populares do Brasil, é um dos primeiros a vocalizarem ao amanhecer.",
+        picture_source: "https://commons.wikimedia.org/wiki/File:Pitangus_sulphuratus_3.jpg#/media/Ficheiro:Pitangus_sulphuratus_3.jpg",
+        picture: "file:///D:/Curso%20Programador%20BR%20interface%20web/outros/jogo%20forca/library/bem-te-vi.jpg",
+        binomen: "Pitangus sulphuratus",
+
+        
+        
+    }, {
+
+        index: "2",
+        name: "BABUÍNO",
+        type: "land",
+        info_source: "https://pt.wikipedia.org/wiki/Babu%C3%ADno",
+        info: " Ao contrário dos macacos, os babuínos passam a maior parte do tempo no chão. Suas caudas não são preênseis.<br><br> Os babuínos são grandes lutadores e demonstram pouco medo de outros animais, inclusive seres humanos. Todos têm hierarquias fortes e complexas dentro dos grupos familiares.",
+        picture_source: "https://commons.wikimedia.org/wiki/File:Papio_hamadryas_eating_an_apple.JPG",
+        picture: "file:///D:/Curso%20Programador%20BR%20interface%20web/outros/jogo%20forca/library/babuino.jpg",
+        binomen: "Papio hamadryas (Babuíno-sagrado)",
+        
+    }, {
+        name: "BACALHAU",
+    }, {
+        name: "BACURI",
+    }, {
+        name: "BADEJO",
+    }, {
+        name: "BAGRE",
+    }, {
+        name: "BABUÍNO",
+    }, {
+        name: "BALEIA",
+    }, {
+        name: "BARATA",
+    }, {
+        name: "BARBO",
+    }, {
+        name: "BARRACUDA",
+    }, {
+        name: "BEIJA-FLOR",
+    }, {
+        name: "BESOURO",
+    }, {
+        name: "BEZERRO",
+    },
+]
+
+const land = [
+
+    {
+        // criar função
+        // type: "land",
+        // screen: x.style.backgroundcolor = "beige",
+        // test: console.log(this)
+    },
+    
+    // {
+    //     type: "air",
+    //     screen_color: "rgb(103, 173, 230)",
+    //     name_color: "DodgerBlue",
+    //     card_color: "cornsilk",
+    // }
+]
+// function create_cards () {
+//     console.log(library.this)
+//     console.log(this)
+// }
+
+
+console.log(library)
+// console.log(BEM_TE_VI)
+// create_cards: function(){
+
+//     console.log(library.this)
+// }
